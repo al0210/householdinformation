bdc857d559ee4f8fcf8d5740590006cb2f67047c
al0210 <77442365+al0210@users.noreply.github.com>
2022-10-25 19:07:24 +0530

    Update task_2 MySQL.py

--- Datastore/task_2 MySQL.py
+++ Datastore/task_2 MySQL.py
@@ -5,24 +5,31 @@ Created on Thu Oct 20 14:51:25 2022
 @author: al021
 """
 
-import csv
-import MySQLdb
+import csv      
+# library to import .csv file which needs to be connected with the database 
+import MySQLdb     
+# importing library - MySQLdb to connect with the database and write the necessary data
 
 mydb = MySQLdb.connect(host="127.0.0.1", user = "root", password = "", database="household_db")
+# object - mydb defined to created a connection with the database in the given host, user and password
 
-with open('task_2_data.csv') as csv_file:
-    csvfile = csv.reader(csv_file, delimiter =',')
-    table_value = []
+with open('task_2_data.csv') as csv_file:           # to open the .csv that needs to be connected to the database
+    csvfile = csv.reader(csv_file, delimiter =',')       # the .csv which is opened is being read into the object - csvfile
+    table_value = []                 # variable to store the data read from the .csv file
     for row in csvfile:
         row_value = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12],
                      row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24], row[25],
                      row[26], row[27], row[28], row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36], row[37], row[38],
                      row[39], row[40], row[41], row[42], row[43], row[44], row[45], row[46], row[47], row[48], row[49], row[50], row[51],
                      row[52], row[53], row[54], row[55], row[56], row[57])
-        table_value.append(row_value)
+        table_value.append(row_value)           # the data from the .csv file is appended into table_value rowwise
         
 query = "insert into `table_household`(`region`, `household_id`, `age_M1`, `age_M2`, `age_M3`, `age_M4`, `age_M5`, `age_M6`, `age_M7`, `age_M8`, `age_M9`, `age_M10`, `gender_M1`, `gender_M2`, `gender_M3`, `gender_M4`, `gender_M5`, `gender_M6`, `gender_M7`, `gender_M8`, `gender_M9`, `gender_M10`, `income_M1`, `income_M2`, `income_M3`, `income_M4`, `income_M5`, `income_M6`, `income_M7`, `income_M8`, `income_M9`, `income_M10`, `expenditure_cooked_food`, `expenditure_house`, `expenditure_electricity`, `expenditure_water`, `expenditure_sanitation`, `expenditure_waste_collection`, `expenditure_cooking_fuel`, `expenditure_internet`, `expenditure_communication`, `expenditure_other`, `expenditure_transport`, `priority_groceries`, `priority_cooked_food`, `priority_education`, `priority_house`, `priority_health`, `priority_transport`, `priority_electricity`, `priority_water`, `priority_sanitation`, `priority_waste_collection`, `priority_cooking_fuel`, `priority_internet`, `priority_communication`, `priority_loan_repayment`, `priority_other`) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+# to insert the given data into the designated table
 
 mycursor = mydb.cursor()
-mycursor.executemany(query, table_value)
+# defining the cursor in the database to perform the required executions
+dbcursor.executemany(query, table_value)
+# executed the operation to store the data in table_value into the database
 mydb.commit()
+# to confirm the changes made into the database
